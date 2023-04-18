1724bfd04ace60c7ca36a47b1da2c6614cfe9b99
kmax.eth <max.kaneco@gmail.com>
2023-04-18 12:59:54 +0800

    Expose mysql Conn struct

--- database/mysql/config.go
+++ database/mysql/config.go
@@ -2,14 +2,14 @@ package mysql
 
 // Config defines mysql configuration.
 type Config struct {
-	Master       connection   `yaml:"master"`
-	Slaves       []connection `yaml:"slaves"`
-	MaxOpenConns int          `yaml:"max_open_conns"`
-	MaxIdleConns int          `yaml:"max_idle_conns"`
-	LogLevel     string       `yaml:"log_level"`
+	Master       Conn   `yaml:"master"`
+	Slaves       []Conn `yaml:"slaves"`
+	MaxOpenConns int    `yaml:"max_open_conns"`
+	MaxIdleConns int    `yaml:"max_idle_conns"`
+	LogLevel     string `yaml:"log_level"`
 }
 
-type connection struct {
+type Conn struct {
 	Host     string `yaml:"host"`
 	Port     uint   `yaml:"port"`
 	Username string `yaml:"username"`
